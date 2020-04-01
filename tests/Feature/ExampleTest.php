dc4720953ec3daf660dfa49d419157d877ca2dfd
Jeremy Sarda <jcsarda@gmail.com>
2020-03-31 23:47:08 -0700

    Removed Basic Test Case

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,10 +12,10 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+    // public function testBasicTest()
+    // {
+    //     $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
+    //     $response->assertStatus(200);
+    // }
 }
