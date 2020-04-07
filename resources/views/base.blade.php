ec8c004a2f4ea63c5eb8a4069a67997e25429665
Jeremy Sarda <jcsarda@gmail.com>
2020-04-07 12:22:55 -0700

    Certificate / Bill Design

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,10 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-12">
-            <scales-of-justice></scales-of-justice>
+
+<div class="money-thin-border">
+    <div class="money-thick-border-start">
+
+        <h1 class="money-caps-font-tan">
+            JOHN S. ONEY IV
+        </h1>
+
+        <div class="money-thick-border-end">
+            <div class="container">
+
+                <div class="row justify-content-center">
+                    <div class="col-12">
+
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-3">
+
+                    </div>
+                    <div class="col-6 text-center">
+                        <scales-of-justice></scales-of-justice>
+                    </div>
+                    <div class="col-3">
+
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-3">
+                        <img src="/assets/illustrations/jo-cutout-1.svg" style="border-radius: 50%;border-top:50px solid #aaa;" />
+                    </div>
+                    <div class="col-6 text-center">
+                        <h1 class="money-caps-font-dark">
+                            ONEY
+                        </h1>
+                    </div>
+                    <div class="col-3">
+                        <img src="/assets/illustrations/jo-cutout-1.svg" style="border-radius: 50%;border-top:50px solid #aaa;" />
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     </div>
 </div>
